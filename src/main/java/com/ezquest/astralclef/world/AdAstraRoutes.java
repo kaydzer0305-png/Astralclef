7f2f1f9282e0985a7c85b18a3f66cf2b33dd4f90
kaydzer0305-png <kaydzer0305@gmail.com>
2026-09-05 06:10:14 -0700

    scaffold: add AdAstraRoutes.java stub

---
+++ src/main/java/com/ezquest/astralclef/world/AdAstraRoutes.java
@@ -0,0 +1,10 @@
+package com.ezquest.astralclef.world;
+
+/**
+ * Stub: Ad Astra travel / planet routing helpers (Moon -> Mars -> Mercury).
+ */
+public class AdAstraRoutes {
+	public void planRoute() {
+		// TODO: implement Ad Astra route planning
+	}
+}
