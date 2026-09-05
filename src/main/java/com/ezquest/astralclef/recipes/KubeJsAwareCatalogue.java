b1f907026b502d256353c3815b7df8934af1f5db
kaydzer0305-png <kaydzer0305@gmail.com>
2026-09-05 06:10:06 -0700

    scaffold: add KubeJsAwareCatalogue.java stub

---
+++ src/main/java/com/ezquest/astralclef/recipes/KubeJsAwareCatalogue.java
@@ -0,0 +1,10 @@
+package com.ezquest.astralclef.recipes;
+
+/**
+ * Stub: recipe catalogue aware of KubeJS-generated / overridden recipes.
+ */
+public class KubeJsAwareCatalogue {
+	public void refresh() {
+		// TODO: scan and index KubeJS-aware recipes
+	}
+}
