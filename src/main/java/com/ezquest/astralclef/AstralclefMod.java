563c9942c54db5020a45610d657d7170a8aa9ccf
kaydzer0305-png <kaydzer0305@gmail.com>
2026-09-05 06:16:14 -0700

    feat(ch01): init TaskRunner, CreateRecipeKinds, and commands on load

--- src/main/java/com/ezquest/astralclef/AstralclefMod.java
+++ src/main/java/com/ezquest/astralclef/AstralclefMod.java
@@ -1,15 +1,26 @@
 package com.ezquest.astralclef;
 
+import com.ezquest.astralclef.command.AstralCommands;
+import com.ezquest.astralclef.recipes.KubeJsAwareCatalogue;
+import com.ezquest.astralclef.task.TaskRunner;
+import com.ezquest.astralclef.tasks.create.CreateRecipeKinds;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AstralclefMod implements ModInitializer {
-    public static final String MOD_ID = "astralclef";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final String MOD_ID = "astralclef";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-  @Override
-    public void onInitialize() {
-          LOGGER.info("Astralclef loaded");
-    }
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Astralclef loaded");
+
+		TaskRunner.getInstance();
+		CreateRecipeKinds.init();
+		KubeJsAwareCatalogue.getInstance().refresh();
+		AstralCommands.register();
+
+		LOGGER.info("TaskRunner + CreateRecipeKinds + command hooks initialized (Ch0.5–1 ready)");
+	}
 }
