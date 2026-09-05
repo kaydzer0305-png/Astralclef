b8148b9aaeaf4a91d1a114da5c897c1091f64d19
kaydzer0305-png <kaydzer0305@gmail.com>
2026-09-05 06:10:07 -0700

    scaffold: add CreateRecipeKinds.java stub

---
+++ src/main/java/com/ezquest/astralclef/tasks/create/CreateRecipeKinds.java
@@ -0,0 +1,13 @@
+package com.ezquest.astralclef.tasks.create;
+
+/**
+ * Stub: catalogue of Create mod recipe kinds used by Astralclef task planning
+ * (sequenced assembly, mechanical crafting, filling, basins).
+ */
+public final class CreateRecipeKinds {
+	private CreateRecipeKinds() {}
+
+	public static void init() {
+		// TODO: register Create recipe kind handlers
+	}
+}
