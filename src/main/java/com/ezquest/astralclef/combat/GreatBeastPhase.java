049bc6bf127eae790859b16af2e1f466107958a9
kaydzer0305-png <kaydzer0305@gmail.com>
2026-09-05 06:10:03 -0700

    scaffold: add GreatBeastPhase.java stub

---
+++ src/main/java/com/ezquest/astralclef/combat/GreatBeastPhase.java
@@ -0,0 +1,10 @@
+package com.ezquest.astralclef.combat;
+
+/**
+ * Stub: Great Beast combat phase handling (Ch6).
+ */
+public class GreatBeastPhase {
+	public void engage() {
+		// TODO: implement Great Beast combat logic
+	}
+}
