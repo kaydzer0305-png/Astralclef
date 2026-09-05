375e30ff27b56a69199cb7022da3a4a786ec27de
kaydzer0305-png <kaydzer0305@gmail.com>
2026-09-05 06:10:05 -0700

    scaffold: add AstralCommands.java stub

---
+++ src/main/java/com/ezquest/astralclef/command/AstralCommands.java
@@ -0,0 +1,12 @@
+package com.ezquest.astralclef.command;
+
+/**
+ * Stub: Astralclef command registration (@astral / phase runners).
+ */
+public final class AstralCommands {
+	private AstralCommands() {}
+
+	public static void register() {
+		// TODO: register /astralclef commands
+	}
+}
