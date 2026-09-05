44b56624bd0e9a2b103fb89b989c47d842812b19
kaydzer0305-png <kaydzer0305@gmail.com>
2026-09-05 06:10:12 -0700

    scaffold: add ChMercuryTask.java stub

---
+++ src/main/java/com/ezquest/astralclef/tasks/phases/ChMercuryTask.java
@@ -0,0 +1,10 @@
+package com.ezquest.astralclef.tasks.phases;
+
+/**
+ * Stub: Mercury phase progression.
+ */
+public class ChMercuryTask {
+	public void run() {
+		// TODO: implement Mercury phase
+	}
+}
