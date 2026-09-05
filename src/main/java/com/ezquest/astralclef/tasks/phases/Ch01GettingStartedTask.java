6707ae1ea67c7592f92f0184a96e93ec706b3a7b
kaydzer0305-png <kaydzer0305@gmail.com>
2026-09-05 06:10:08 -0700

    scaffold: add Ch01GettingStartedTask.java stub

---
+++ src/main/java/com/ezquest/astralclef/tasks/phases/Ch01GettingStartedTask.java
@@ -0,0 +1,10 @@
+package com.ezquest.astralclef.tasks.phases;
+
+/**
+ * Stub: Chapter 0.5-1 getting-started phase (early Create + Astral basics).
+ */
+public class Ch01GettingStartedTask {
+	public void run() {
+		// TODO: implement Ch0.5-1 progression
+	}
+}
